557edec36a7163902c9cafca72620f0790d49307
ironlionm4n <mikailsmiller@gmail.com>
2022-08-03 08:10:45 -0400

    automatic context generation, simple styling

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,45 +1,47 @@
 import React, { createContext, useReducer, useState } from 'react'
+import createDataContext from './createDataContext'
 
-const BlogContext = createContext()
 const ADD_BLOG = 'ADD'
 const DELETE_BLOG = 'DELETE'
 
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'ADD': {
+
       return [...state, { title: `Blog # ${state.length + 1}` }]
     }
     case 'DELETE': {
-      return [state.filter((blog) => blog.title !== `Blog # ${state.length}`)]
+      const temp = state.filter(
+        blog => blog.title !== `Blog # ${state.length}`
+      )
+
+      console.log(temp)
+
+        // for(let i = 0; i < state.length; i++) {
+        //   console.log(state[i].title === temp[i].title)
+        //   console.log(temp[i].title)
+        // }
+      return temp
     }
     default:
       return state
   }
 }
 
-export const BlogProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(blogReducer, [])
-
-  const addBlogPost = () => {
+const addBlogPost = dispatch => {
+  return () => {
     dispatch({ type: ADD_BLOG })
   }
-
-  const deleteBlogPost = () => {
-    dispatch({ type: DELETE_BLOG })
-  }
-
-  return (
-    <BlogContext.Provider
-      value={{
-        data: state,
-        addBlogPost: addBlogPost,
-        deleteBlogPost: deleteBlogPost
-      }}
-    >
-      {/* components are passed down from App to custom components via the children prop */}
-      {children}
-    </BlogContext.Provider>
-  )
 }
 
-export default BlogContext
+const deleteBlogPost = dispatch => {
+  return () => {
+    dispatch({ type: DELETE_BLOG })
+  }
+}
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost: addBlogPost, deleteBlogPost: deleteBlogPost },
+  []
+)
---
+++ src/context/createDataContext.js
@@ -0,0 +1,30 @@
+import React, { useReducer } from 'react'
+
+// Create a new data context automatically
+export default (reducer, actions, initialState) => {
+  const Context = React.createContext()
+
+  const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    // actions = { addBlogPost: (dispatch) => { return () => {} } }
+    const boundActions = {}
+    for (let key in actions) {
+      // key === 'addBlogPost'
+      boundActions[key] = actions[key](dispatch)
+    }
+
+    return (
+      <Context.Provider
+        value={{
+          state: state /* can also write as just {{ state }} */,
+          ...boundActions
+        }}
+      >
+        {children}
+      </Context.Provider>
+    )
+  }
+
+  return { Context, Provider }
+}
